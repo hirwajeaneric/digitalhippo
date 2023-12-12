948ad7f2cf8d566d1096b79e0ec370d294fa69f0
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-12 18:43:42 +0200

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import Link from "next/link";
 const perks = [
   {
     name: "Instant Delivery",
-    icon:   ArrowDownToLine,
+    Icon:   ArrowDownToLine,
     description: "Get cement delivered to your doorway in seconds and keep working without worries."
   },
   {
     name: "Guaranteed Quality",
-    icon:   CheckCircle,
+    Icon:   CheckCircle,
     description: "Every cement sold on our platform is verified and certified by the Rwanda Standard Board."
   },
   {
     name: "For the Planet",
-    icon:   Leaf,
+    Icon:   Leaf,
     description: "We've pledged 1% of sales to the preservation and restoration of environment."
   },
 ];
@@ -51,16 +51,16 @@ export default function Home() {
                 className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
                 <div className="md:flex-shrink-0 flex justify-center">
                   <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
-                    {<perk.Icon className="w-1/3 h-1/3" />}
+                    {<perk.Icon className='w-1/3 h-1/3' />}
                   </div>
                 </div>
                 
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
                   <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
                 </div>
-                <p className="mt-3 text-sm text-muted-foreground">
-                  {perk.description}
-                </p>
               </div>
             ))}
           </div>
