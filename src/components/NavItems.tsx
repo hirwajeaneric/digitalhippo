709823c8fff940bb9a3838b53e0d3938d633d2b6
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-13 08:14:33 +0200

    #1. Navigation items
    Working on the navigation items. For this time, it's the desktop navigation which is available. I am also going to add the dropdown menue items

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -7,10 +7,12 @@ import NavItem from "./NavItem";
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+    const isAnyOpen = activeIndex !== null;
+
     return (
         <div className="flex gap-4 h-full">
             {PRODUCT_CATEGORIES.map((category, i) => {
-                
+
                 const handleOpen = () => {
                     if (activeIndex === i) {
                         setActiveIndex(null);
@@ -22,7 +24,13 @@ const NavItems = () => {
                 const isOpen = i === activeIndex;
 
                 return (
-                    <NavItem />
+                    <NavItem 
+                        key={category.value}
+                        category={category} 
+                        handleOpen={handleOpen} 
+                        isOpen={isOpen}
+                        isAnyOpen={isAnyOpen} 
+                    />
                 )
             })};
         </div>
